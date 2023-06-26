f7a986f15e40449c5e4b0f5f25b658d555dc3f75
fab85270 <coutanceauf85@gmail.com>
2023-06-26 22:49:36 +0200

    suppressionplantage

--- project/src/App.js
+++ project/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React from "react";
 import HomePage from './pages/HomePage';
-import PageConnect from './pages/PageConnect';
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/connect" element={<PageConnect/>} />
       </Routes>
     </Router>
   );
