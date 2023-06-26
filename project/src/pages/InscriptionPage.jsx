28ee6675ee919d1f7bfdb7f90601c459aedb889b
fab85270 <coutanceauf85@gmail.com>
2023-06-26 23:31:15 +0200

    redirectionMenuConnection

--- project/src/pages/InscriptionPage.jsx
+++ project/src/pages/InscriptionPage.jsx
@@ -2,17 +2,23 @@
 import React from 'react';
 import LayoutGlobal from '../Layout/LayoutGlobal';
 import Inscription from '../components/Formulaire/Inscription';
+import { useNavigate } from 'react-router-dom';
 
 
 
 function InscriptionPage(){ 
   
+  const navigate = useNavigate(); 
+
+  const handleButtonClick = () => {
+    navigate('/login');
+  };
   
   return (      
     <LayoutGlobal children={
       <>
         <Inscription/>
-        <p>test</p>
+        <button onClick={handleButtonClick}> Vous possédez déjà un compte ? </button>
       </>
     }></LayoutGlobal>
       )    
@@ -20,3 +26,4 @@ function InscriptionPage(){
 export default InscriptionPage;
 
   
+
