b9fd79e0bc93fd05691df2ef04549239cadab73c
fab85270 <coutanceauf85@gmail.com>
2023-06-27 23:38:27 +0200

    interfaceVoitureLocation

--- project/src/components/Formulaire/ChoixVehicule.jsx
+++ project/src/components/Formulaire/ChoixVehicule.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import boiteManuelle from '../../Images/voitureManuelle.png';
 import boiteAuto from '../../Images/voitureAuto.png';
+import './ChoixVehicule.css';
 
 
 function ChoixVehicule(){
     
   /* Definition des constantes */
   const [selectedImage, setSelectedImage] = useState(null);
-  const voitureManuelle = 'https://fr.freepik.com/https://img.freepik.com/vecteurs-premium/sourire-dessin-anime-voiture-rouge-blanc_29190-4845.jpg?w=2000photos-vecteurs-libre/voiture-dessin-anime';
-  const voitureAuto = 'https://fr.vecteezy.com/art-vectoriel/3236135-dessin-anime-voiture-coulehttps://static.vecteezy.com/ti/vecteur-libre/p3/3236135-dessin-anime-voiture-couleur-brillante-illustration-pour-enfants-gratuit-vectoriel.jpgur-brillante-illustration-pour-enfants';
+  
   
 
   /* Méthode qui va selectionner l'image quand on aura cliqué dessus*/
@@ -24,25 +24,27 @@ function ChoixVehicule(){
   };
 
   return (
-    <div>
-      <h1>Choisir un des deux modèles de voiture : manuelle ou automatique</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="image-container">
-          <img
-            src={boiteAuto}
-            alt="Voiture Automatique"
-            className={selectedImage === 'image1' ? 'selected' : ''}
-            onClick={() => handleImageClick('image1')}
-          />
-          <img
-            src={boiteManuelle}
-            alt="Voiture Manuelle"
-            className={selectedImage === 'image2' ? 'selected' : ''}
-            onClick={() => handleImageClick('image2')}
-          />
-        </div>
-        <button type="submit">Soumettre votre choix</button>
-      </form>
+    <div className="container">
+      <div className="centered">
+        <h1>Choisir un modèle de voiture : manuel ou automatique</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="image-container">
+            <img
+              src={boiteAuto}
+              alt="Voiture Automatique"
+              className={selectedImage === 'image1' ? 'selected' : 'image'}
+              onClick={() => handleImageClick('image1')}
+            />
+            <img
+              src={boiteManuelle}
+              alt="Voiture Manuelle"
+              className={selectedImage === 'image2' ? 'selected' : 'image'}
+              onClick={() => handleImageClick('image2')}
+            />
+          </div>
+          <button type="submit">Soumettre votre choix</button>
+        </form>
+      </div>
     </div>
   );
 };
