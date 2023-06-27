318133e4e93dbac13ad5f6de621d968a55bf96fa
fab85270 <coutanceauf85@gmail.com>
2023-06-27 21:49:08 +0200

    ChoixVehicule:Components

---
+++ project/src/pages/VoiturePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import LayoutGlobal from '../Layout/LayoutGlobal';
+import ChoixVehicule from '../components/Formulaire/ChoixVehicule';
+
+
+function VoiturePage(){ 
+  
+  
+  return (      
+    <LayoutGlobal children={
+        <>   
+             <ChoixVehicule/>
+        </>       
+    }></LayoutGlobal>
+      )    
+    } 
+export default VoiturePage;
+
+  
