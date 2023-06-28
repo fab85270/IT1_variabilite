d3306ed899aa6c09f0f51dcfd095750f9f9e42ee
fab85270 <coutanceauf85@gmail.com>
2023-06-28 20:49:49 +0200

    maxenceTNul

--- project/src/components/VerificationPermis/verificationPermis.jsx
+++ project/src/components/VerificationPermis/verificationPermis.jsx
@@ -1,57 +1,45 @@
 import React, { useState,useContext, useEffect } from 'react';
-import Tesseract from 'tesseract.js';
 import { PermisContext } from '../../Context/PermisContext';
+import { useNavigate } from 'react-router-dom';
 import './verificationPermis.css'
 
 const VerificationPermis = () => {
-  const [text, setText] = useState('');
+
+  /* Definition des constantes */
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
-
   const{permis,setPermis} = useContext(PermisContext);
-  console.log("dans permis",permis);
-  const handleImageUpload = (e) => {
-    const file = e.target.files[0];
-    setLoading(true);
-    if (file) {
-        setLoading(true);
-        setText("Permis validé")
-        setPermis(true);
-        console.log("hbafiueahbfdiauzhb",permis);
-        setLoading(false);
-        try{
-            //const { data } = await Tesseract.recognize(file, 'eng');
-            //rajouter fonctionnalité lorsque permis est validé rajouter une ligne dans profil
-            //Si on ne trouve pas de vrai IA "Simulation de permis validé"
-            //setText(data.text);
-            setText("Permis validé")
-            setPermis(true);
-            console.log("hbafiueahbfdiauzhb",permis);
-            setLoading(false);
-        }catch(error){
-            setText('Erreur')
-            setError('Erreur lors de la reconnaissance de l\'image. Veuillez réessayer avec une autre image.')
-            setLoading(false);
-            setPermis(false);
-            alert("viens pas ici")
-        }
-    }
-    alert('Permis => '+permis);
-  };
 
-  useEffect(() => {
-    // This code will execute after the state update
-    console.log("permis:", permis);
-  }, [permis])
+  /* Initialisation de la redirection sans rafraichissement */
+  const navigate = useNavigate(); 
+
+  const handleImageUpload = (e) => {
+
+    setLoading(true); // La durée de chargement de l'image. 
+
+    const file = e.target.files[0];
+
+    if (file) { // Vérification que le peris a bien été importé 
+        setPermis(true);
+
+          setTimeout(() => { //permet de faire genre on a chargement de 3 secondes de l'image
+          setLoading(false);
+         
+        }, 3000); 
+
+        setTimeout(() => { //permet de faire genre on a chargement de 5 secondes avant redirection
+          setLoading(false);
+          navigate('/voitureLocation');
+        }, 8000); 
+        } 
+    }
   return (
     <div id='box'>
         <div id='verifPermis'>
             <input type="file" accept="image/*" onChange={handleImageUpload} />
         </div>
         <div id='etat'>
-            {loading && <div id='chargement'>Chargement...</div>}
-            {error && <div className="error">{error}</div>}
-            {permis && <div id='permisValide'>{text}<br/><img src="https://cdn2.iconfinder.com/data/icons/happy-running-errands-3/512/happy_running_errands_drive-512.png"></img></div>}
+            <div>{loading && <div id='chargement'>Chargement...</div>}</div>
+            <div> {permis && !loading && <div id='permis'><br/>Votre permis est valide, vous allez être redirigé vers la page de location dans 5 secondes<img src="https://cdn2.iconfinder.com/data/icons/happy-running-errands-3/512/happy_running_errands_drive-512.png"></img></div>}</div>
         </div>
     </div>
     
