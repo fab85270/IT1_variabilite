45a6a2800595b2288a1e22efb2dab30dd9a01fc1
Maxence <maxencebodo@hotmail.fr>
2023-06-28 13:44:07 +0200

    Mise en place context permis

--- project/src/App.js
+++ project/src/App.js
@@ -11,6 +11,7 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import {UserContextProvider} from './Context/UserContext';
 import {ConnectionContextProvider} from './Context/ConnectionContext';
 import {PriceContextProvider} from './Context/PriceContext';
+import { PermisContext } from './Context/PermisContext';
 
 function App() {
 
@@ -23,17 +24,19 @@ function App() {
   <UserContextProvider>
     <ConnectionContextProvider>
       <PriceContextProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/inscription" element={<InscriptionPage />} />
-            <Route path="/veloLocation" element={<VeloPage />} />
-            <Route path="/voitureLocation" element={<VoiturePage />}/>
-            <Route path ="/paymentPage" element = {<PaymentPage/>}/>
-            <Route path ="/VerificationPermis" element = {<VerificationPermisPage/>}/>
-          </Routes>
-        </Router>
+        <PermisContext>
+          <Router>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/inscription" element={<InscriptionPage />} />
+              <Route path="/veloLocation" element={<VeloPage />} />
+              <Route path="/voitureLocation" element={<VoiturePage />}/>
+              <Route path ="/paymentPage" element = {<PaymentPage/>}/>
+              <Route path ="/VerificationPermis" element = {<VerificationPermisPage/>}/>
+            </Routes>
+          </Router>
+        </PermisContext>
       </PriceContextProvider>
     </ConnectionContextProvider>
   </UserContextProvider>
