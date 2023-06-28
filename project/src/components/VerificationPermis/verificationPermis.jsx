e526b81f7b4f0e6577a664daa5ee033ee5a52c2c
Skygal44 <58173147+Skygal44@users.noreply.github.com>
2023-06-28 19:14:54 +0200

    babou

--- project/src/components/VerificationPermis/verificationPermis.jsx
+++ project/src/components/VerificationPermis/verificationPermis.jsx
@@ -19,22 +19,22 @@ const VerificationPermis = () => {
         setPermis(true);
         console.log("hbafiueahbfdiauzhb",permis);
         setLoading(false);
-        // try{
-        //     //const { data } = await Tesseract.recognize(file, 'eng');
-        //     //rajouter fonctionnalité lorsque permis est validé rajouter une ligne dans profil
-        //     //Si on ne trouve pas de vrai IA "Simulation de permis validé"
-        //     //setText(data.text);
-        //     setText("Permis validé")
-        //     setPermis(true);
-        //     console.log("hbafiueahbfdiauzhb",permis);
-        //     setLoading(false);
-        // }catch(error){
-        //     setText('Erreur')
-        //     setError('Erreur lors de la reconnaissance de l\'image. Veuillez réessayer avec une autre image.')
-        //     setLoading(false);
-        //     setPermis(false);
-        //     alert("viens pas ici")
-        // }
+        try{
+            //const { data } = await Tesseract.recognize(file, 'eng');
+            //rajouter fonctionnalité lorsque permis est validé rajouter une ligne dans profil
+            //Si on ne trouve pas de vrai IA "Simulation de permis validé"
+            //setText(data.text);
+            setText("Permis validé")
+            setPermis(true);
+            console.log("hbafiueahbfdiauzhb",permis);
+            setLoading(false);
+        }catch(error){
+            setText('Erreur')
+            setError('Erreur lors de la reconnaissance de l\'image. Veuillez réessayer avec une autre image.')
+            setLoading(false);
+            setPermis(false);
+            alert("viens pas ici")
+        }
     }
     alert('Permis => '+permis);
   };
